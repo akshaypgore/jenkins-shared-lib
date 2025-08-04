42a8bec5edffad599a56bef6b4d3d98351a5b400
Akshay Gore <akshay.pradeep.gore@gmail.com>
2025-08-04 11:00:09 +0530

    Add basic files

---
+++ vars/checkoutRepo.groovy
@@ -0,0 +1,12 @@
+def call(String repoUrl, String branch = 'master') {
+    echo "Checking out repository: ${repoUrl} on branch: ${branch}"
+    
+    def checkoutConfig = [
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[url: repoUrl]]
+    ]
+    
+    checkout(checkoutConfig)
+    echo "Repository checkout completed successfully"
+}
