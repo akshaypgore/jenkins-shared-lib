371f9c601fef12905575e553611ad6fabd4d8094
Akshay Gore <akshay.pradeep.gore@gmail.com>
2025-08-04 14:53:25 +0530

    Modify script

--- vars/checkoutRepo.groovy
+++ vars/checkoutRepo.groovy
@@ -1,10 +1,11 @@
 def call(String repoUrl) {
-    echo "Checking out repository: ${repoUrl} on branch: ${branch}"
+    echo "Checking out repository: ${repoUrl} on branch: ${params.TAG}"
     
-    checkout(
+    checkoutConfig = [
         [$class: 'GitSCM',
         branches: [[name: "refs/tags/${params.TAG}"]],
         userRemoteConfigs: [[url: 'https://github.com/jenkinsci/git-parameter-plugin.git']]]
-        )
+    ]
+    checkout(checkoutConfig)
     echo "Repository checkout completed successfully" 
 }
