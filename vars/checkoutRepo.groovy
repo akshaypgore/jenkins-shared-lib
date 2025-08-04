fab905ec6df167600ab464562a4e4a223510767b
Akshay Gore <akshay.pradeep.gore@gmail.com>
2025-08-04 15:06:35 +0530

    Modify script

--- vars/checkoutRepo.groovy
+++ vars/checkoutRepo.groovy
@@ -1,9 +1,13 @@
-def call(String repoUrl, String TAG = "${params.TAG}") {
+def checkoutRepoWithTag(repoUrl, tag_name) {
     
     def checkoutConfig = [
-        [$class: 'GitSCM',
-        branches: [[name: "refs/tags/${TAG}"]],
-        userRemoteConfigs: [[url: 'https://github.com/jenkinsci/git-parameter-plugin.git']]]
+        $class: 'GitSCM',
+        branches: [
+            [name: "refs/tags/${tag_name}"]
+        ],
+        userRemoteConfigs: [
+            [url: 'https://github.com/jenkinsci/git-parameter-plugin.git']
+        ]
     ]
     checkout(checkoutConfig)
 
